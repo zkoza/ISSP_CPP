6a84b29ef02b02cb8d91dd28a4da9eb5e552e095
Zbigniew Koza <zbigniew.koza@ift.uni.wroc.pl>
2016-04-04 02:58:28 +0200

    + wektor

---
+++ zadania/programy/wektor/wektor.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include "wektor.h"                  // włączenie interfejsu klasy
+
+// Poniższą implementację można i trzeba zoptymalizować,
+// żeby niepotrzebnie nie wywoływać pary operatorów new/delete
+Wektor & Wektor::operator= (Wektor const& wek)
+{
+    std::cout << __FUNCTION__ << " [copy]\n"; // diagnostyka
+
+    if (this == &wek)                // czy kopiujemy obiekt na samego siebie?
+        return *this;
+
+    delete [] _ptab;                 // zwolnienie pamięci
+    _rozmiar = wek._rozmiar;         // skopiowanie rozmiaru
+    _ptab = new int [_rozmiar];      // zarezerwowanie nowej pamięci
+
+    for (size_t i = 0; i < _rozmiar; i++)  // skopiowanie danych
+        _ptab[i] = wek._ptab[i];
+    return *this;                    // zwrócenie referencji "do siebie"
+}
+
+
+Wektor::Wektor(size_t n, int value)
+: _rozmiar(n),
+  _ptab(new int[n])
+{
+    std::cout << "ctor [n]\n"; // diagnostyka
+
+    for (int i = 0; i < (int)_rozmiar; i++)
+        _ptab[i] = value;
+}
+
+
+Wektor::Wektor(Wektor const& w)
+    :  _rozmiar (w._rozmiar),
+       _ptab(0)
+{
+    std::cout << "ctor [copy]\n";
+
+    _ptab = new int [_rozmiar];
+    for (size_t i = 0; i < _rozmiar; i++)  // skopiowanie danych
+        _ptab[i] = w._ptab[i];
+}
+
+
+Wektor & operator+= (Wektor & lhs, Wektor const& rhs)
+{
+    std::cout << __FUNCTION__ << "\n";
+
+    if (lhs.size() != rhs.size())
+        throw std::string(__FUNCTION__) + ": sizes don't match";
+
+    for (int i = 0; i < (int)lhs.size(); i++)
+        lhs[i] += rhs[i];
+
+    return lhs;
+}
+
+
+Wektor operator+(Wektor lhs, Wektor const& rhs)
+{
+    if (lhs.size() != rhs.size())
+        throw std::string(__FUNCTION__) + ": sizes don't match";
+
+    lhs += rhs;
+    return lhs;
+}
+
+
+void add3(Wektor & target, Wektor const& v1, Wektor const& v2, Wektor const& v3)
+{
+    if (target.size() != v1.size() || v1.size() != v2.size() || v2.size() != v3.size())
+        throw std::string(__FUNCTION__) + ": sizes don't match";
+
+    for (int i = 0; i < (int)target.size(); i++)
+    {
+        target[i] = v1[i] + v2[i] + v3[i];
+    }
+}
+
+
+#if __cplusplus >= 201103L
+
+Wektor &  Wektor::operator= (Wektor && wek)
+{
+    std::cout << __FUNCTION__ << " [move]\n";
+    if (this == &wek)                // czy kopiujemy obiekt na samego siebie?
+        return *this;
+
+    delete [] _ptab;           // zwolnienie pamięci
+    _rozmiar = wek._rozmiar;   // skopiowanie rozmiaru
+    _ptab = wek._ptab;         // "przeniesienie" pamięci
+    wek._ptab = nullptr;       // wyzerowanie oryginału
+    wek._rozmiar = 0;          // wyzerowanie oryginału
+    return *this;
+}
+
+
+Wektor::Wektor(Wektor && w)
+    : _rozmiar(w._rozmiar),
+      _ptab (w._ptab)
+{
+    std::cout << "ctor [move]\n";
+
+    w._rozmiar = 0;
+    w._ptab = nullptr;
+}
+
+
+Wektor operator+(Wektor const& lhs, Wektor && rhs)
+{
+    if (lhs.size() != rhs.size())
+        throw std::string(__FUNCTION__) + ": sizes don't match";
+
+    rhs += lhs;
+    return rhs;
+}
+#endif
+
+
+
---
+++ zadania/programy/wektor/wektor.h
@@ -0,0 +1,36 @@
+#include <cstdlib>     // <-- żeby kompilator rozpoznawał typ size_t
+
+class Wektor
+{
+private:
+    size_t _rozmiar;
+    int *  _ptab;
+
+public:
+    Wektor(size_t n, int value = 0);
+    Wektor(Wektor const& w);
+#if __cplusplus >= 201103L
+    Wektor(Wektor && w);
+#endif
+    ~Wektor()                       { delete [] _ptab; }
+
+    size_t size() const             { return _rozmiar; }
+    int & operator [](int n)        { return _ptab[n]; }
+    int   operator [](int n) const  { return _ptab[n]; }
+
+    Wektor & operator= (Wektor const& wek);  // <-- deklaracja metody
+
+#if __cplusplus >= 201103L
+    Wektor &  operator= (Wektor && wek);  // <-- deklaracja metody
+#endif
+};
+
+
+Wektor & operator+= (Wektor & lhs, Wektor const& rhs);
+Wektor   operator+  (Wektor lhs,   Wektor const& rhs);
+
+void add3(Wektor & target, Wektor const& v1, Wektor const& v2, Wektor const& v3);
+
+#if __cplusplus >= 201103L
+Wektor   operator+ (Wektor const& lhs, Wektor && rhs);
+#endif
